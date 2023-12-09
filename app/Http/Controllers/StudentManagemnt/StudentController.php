573998606e82a979e6b1054807f1cb56206e0be7
Santhosh-Raiser <santhoshsv358@gmail.com>
2023-12-09 19:16:07 +0530

    santhosh commit

--- app/Http/Controllers/StudentManagemnt/StudentController.php
+++ app/Http/Controllers/StudentManagemnt/StudentController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Student;
+namespace App\Http\Controllers\StudentManagemnt;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
---
+++ app/Http/Controllers/TeacherManagemnt/TeacherController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\TeacherManagemnt;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TeacherController extends Controller
+{
+    //
+}
